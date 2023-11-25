6bf0b6399d3be32b9631ff13452df20a8d4a402d
Faisal25DEC <faisalmd25121999@gmail.com>
2023-11-26 02:16:02 +0530

    final commit

--- src/components/top-banner.jsx
+++ src/components/top-banner.jsx
@@ -2,19 +2,30 @@ import React from "react";
 import "../App.css";
 import TopBannerImage from "../assets/top-banner.jpg";
 import TopBannerImage2 from "../assets/top-banner-2.png";
-
+import Cards from "../assets/tb-1-1.png";
+import TopBannerImage3 from "../assets/tb-1-2.png";
 const TopBanner = () => {
   return (
-    <div className="img-shadow h-[65vh]">
+    <div className="img-shadow h-[67vh] lg:h-[63vh]">
       <img
         src={TopBannerImage}
         alt=""
-        className="w-full rounded-xl h-[40vh] object-cover"
+        className=" w-full rounded-xl h-[37.5vh] object-cover"
       />
       <img
         src={TopBannerImage2}
         alt=""
-        className="w-full h-[20vh] mt-[1rem] object-cover"
+        className="hidden lg:block w-full h-[18.5vh] mt-[1rem] object-cover"
+      />
+      <img
+        src={Cards}
+        alt=""
+        className="hidden lg:block right-[-0.5rem] bottom-[56vh] w-[12rem] h-[24.5vh] absolute"
+      />
+      <img
+        src={TopBannerImage3}
+        alt=""
+        className="block lg:hidden h-[23vh] w-full mt-[1rem] rounded-lg"
       />
     </div>
   );
