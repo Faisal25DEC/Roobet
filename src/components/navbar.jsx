a360db3bfcc077e299658918514394b2ef2a8b71
Faisal25DEC <faisalmd25121999@gmail.com>
2023-11-26 01:49:42 +0530

    final commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -23,9 +23,9 @@ const Navbar = () => {
       </div>
 
       <div className="flex items-center justify-center md:py-0 py-2">
-        <div className="w-[90%] lg:w-[70%] m-auto flex items-center justify-between">
+        <div className="w-[90%] lg:w-[74%] m-auto flex items-center justify-between">
           <div>
-            <img src={Logo} alt="" className="h-16 hidden md:block" />
+            <img src={Logo} alt="" className="h-14 hidden md:block" />
             <img
               src={MobileLogo}
               alt=""
